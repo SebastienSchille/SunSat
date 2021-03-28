99e2290de7ee9a2703a2b7fa52cc6a7a95a384e0
Sebastien Schille <sebastien.j.schille@gmail.com>
2021-03-28 23:59:06 +0100

    Tabs added for the teams

---
+++ resources/javascript/tabs.js
@@ -0,0 +1,18 @@
+document.getElementById("defaultOpen").click();
+
+function openTeams(evt, team){
+    var i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(team).style.display = "flex";
+    evt.currentTarget.className += " active";
+}
